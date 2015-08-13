6533b4c5e600300cd7c3e776d6bd9f37d3bf8c99
CNwwsun <wwsun@thoughtworks.com>
2015-08-13 14:19:24 +0800

    Init Commit

--- BASE/BASE/Additions/Foundation/NSString+iHAddition.m
+++ BASE/BASE/Additions/Foundation/NSString+iHAddition.m
@@ -48,7 +48,7 @@
 - (NSString *)md5 {
 	const char *concat_str = [self UTF8String];
 	unsigned char result[CC_MD5_DIGEST_LENGTH];
-	CC_MD5(concat_str, strlen(concat_str), result);
+	CC_MD5(concat_str, (CC_LONG)strlen(concat_str), result);
 	NSMutableString *hash = [NSMutableString string];
 	for (int i = 0; i < 16; i++){
 		[hash appendFormat:@"%02X", result[i]];
@@ -65,7 +65,7 @@
     const char *value = [self UTF8String];
     
     unsigned char outputBuffer[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(value, strlen(value), outputBuffer);
+    CC_MD5(value, (CC_LONG)strlen(value), outputBuffer);
     
     NSMutableString *outputString = [[NSMutableString alloc] initWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(NSInteger count = 0; count < CC_MD5_DIGEST_LENGTH; count++){
